da57c4a8e39d8cc37e6a01b62e5d5e075046957d
BeLeap <beleap@beleap.dev>
2023-07-07 09:05:51 +0000

    display username instead of subscription

--- users/beleap/config/starship.nix
+++ users/beleap/config/starship.nix
@@ -2,7 +2,7 @@
   nix_shell.symbol = "(nix)";
   azure = {
     disabled = false;
-    format = "on [$symbol($subscription)]($style) ";
+    format = "on [$symbol($username)]($style) ";
     symbol = "󰠅 ";
     style = "blue bold";
   };
