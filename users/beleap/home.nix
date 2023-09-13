0d80f87f75ef81cf335eb8669cde65ffa54aa86f
BeLeap <beleap@beleap.dev>
2023-09-13 09:36:24 +0900

    fix: remove firefox desktop entries

--- users/beleap/home.nix
+++ users/beleap/home.nix
@@ -56,13 +56,10 @@
       ];
     };
     desktopEntries = {
-      firefox = {
-        name = "Firefox";
-        genericName = "Web Browser";
-        exec = "firefox %U";
+      reboot = {
+        name = "Reboot";
+        exec = "reboot";
         terminal = false;
-        categories = [ "Application" "Network" "WebBrowser" ];
-        mimeType = [ "text/html" "text/xml" ];
       };
     };
   };
