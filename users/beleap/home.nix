4e155886d0b3b052d855e0cb75b3fadb27d27e01
BeLeap <beleap@beleap.dev>
2023-09-20 14:47:36 +0900

    feat: kime better config

--- users/beleap/home.nix
+++ users/beleap/home.nix
@@ -54,6 +54,15 @@ in
         engine = {
           hangul = {
             layout = "sebeolsik-3-90";
+            preedit_johab = "Always";
+            addons = {
+              sebeolsik-3-90 = [
+                "ComposeChoseongSsang"
+                "FlexibleComposeOrder"
+                "ComposeJungseongSsang"
+                "ComposeJongseongSsang"
+              ];
+            };
           };
         };
       };
