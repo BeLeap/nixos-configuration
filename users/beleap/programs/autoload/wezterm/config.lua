38d290617a0e8c25fdd6ef9fcbd7b0ac6130ad68
BeLeap <beleap@beleap.dev>
2023-09-25 15:49:23 +0900

    feat: nanum gothic coding font

--- users/beleap/programs/autoload/wezterm/config.lua
+++ users/beleap/programs/autoload/wezterm/config.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 return {
-  font = wezterm.font("CaskaydiaCove NFM"),
+  font = wezterm.font_with_fallback({ "CaskaydiaCove NFM", "NanumGothicCoding" }),
   font_size = 14.0,
   color_scheme = "Catppuccin Mocha",
   enable_tab_bar = false,
