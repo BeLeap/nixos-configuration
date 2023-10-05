2d14a2672cb381f31172e8f14371902efb1a110f
BeLeap <beleap@beleap.dev>
2023-10-05 14:36:44 +0900

    swaylock color setup

--- users/beleap/config/sway/swaylock/default.nix
+++ users/beleap/config/sway/swaylock/default.nix
@@ -2,5 +2,24 @@
   enable = true;
   settings = {
     color = "1e1e2e";
+
+    line-color = "6c7086";
+    text-color = "cdd6f4";
+    inside-color = "1e1e2e";
+
+    ring-color = "94e2d5";
+    key-hl-color = "74c7ec";
+
+    line-ver-color = "6c7086";
+    text-ver-color = "cdd6f4";
+    inside-ver-color = "1e1e2e";
+
+    ring-ver-color = "f9e2af";
+
+    line-wrong-color = "6c7086";
+    text-wrong-color = "cdd6f4";
+    inside-wrong-color = "1e1e2e";
+
+    ring-wrong-color = "f38ba8";
   };
 }
