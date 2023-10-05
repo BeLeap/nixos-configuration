396b40948cb3abe46c735fa17f9f8f64767f3a58
BeLeap <beleap@beleap.dev>
2023-10-05 21:08:25 +0900

    setup nvidia prime optimus

--- hosts/xps-9510/configuration.nix
+++ hosts/xps-9510/configuration.nix
@@ -23,6 +23,16 @@
 
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 }
 
