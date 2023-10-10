d4443ffeff8ed7b0d26fde331a85ec587f94e3bd
BeLeap <beleap@beleap.dev>
2023-10-10 14:44:03 +0900

    sway colorscheme

--- users/beleap/config/sway/default.nix
+++ users/beleap/config/sway/default.nix
@@ -5,6 +5,34 @@ let
       pkgs
       lib;
   };
+  catppuccin = {
+    Rosewater = "#f5e0dc";
+    Flamingo = "#f2cdcd";
+    Pink = "#f5c2e7";
+    Mauve = "#cba6f7";
+    Red = "#f38ba8";
+    Maroon = "#eba0ac";
+    Peach = "#fab387";
+    Yellow = "#f9e2af";
+    Green = "#a6e3a1";
+    Teal = "#94e2d5";
+    Sky = "#89dceb";
+    Sapphire = "#74c7ec";
+    Blue = "#89b4fa";
+    Lavender = "#b4befe";
+    Text = "#cdd6f4";
+    Subtext1 = "#bac2de";
+    Subtext0 = "#a6adc8";
+    Overlay2 = "#9399b2";
+    Overlay1 = "#7f849c";
+    Overlay0 = "#6c7086";
+    Surface2 = "#585b70";
+    Surface1 = "#45475a";
+    Surface0 = "#313244";
+    Base = "#1e1e2e";
+    Mantle = "#181825";
+    Crust = "#11111b";
+  };
 in
 rec {
   enable = true;
@@ -75,6 +103,45 @@ rec {
       { command = "kime"; }
       { command = "wl-paste --watch cliphist store"; }
     ];
+
+    colors = {
+      background = catppuccin.Base;
+      focused = {
+        border = catppuccin.Lavender;
+        background = catppuccin.Base;
+        text = catppuccin.Text;
+        indicator = catppuccin.Rosewater;
+        childBorder = catppuccin.Lavender;
+      };
+      focusedInactive = {
+        border = catppuccin.Overlay0;
+        background = catppuccin.Base;
+        text = catppuccin.Text;
+        indicator = catppuccin.Rosewater;
+        childBorder = catppuccin.Overlay0;
+      };
+      unfocused = {
+        border = catppuccin.Overlay0;
+        background = catppuccin.Base;
+        text = catppuccin.Text;
+        indicator = catppuccin.Rosewater;
+        childBorder = catppuccin.Overlay0;
+      };
+      urgent = {
+        border = catppuccin.Peach;
+        background = catppuccin.Base;
+        text = catppuccin.Peach;
+        indicator = catppuccin.Overlay0;
+        childBorder = catppuccin.Peach;
+      };
+      placeholder = {
+        border = catppuccin.Overlay0;
+        background = catppuccin.Base;
+        text = catppuccin.Text;
+        indicator = catppuccin.Overlay0;
+        childBorder = catppuccin.Overlay0;
+      };
+    };
   };
  
   extraConfig = ''
