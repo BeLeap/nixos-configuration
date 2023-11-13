d4f0892bdf2308d01b3fb9d657bb01f41e1e4e78
BeLeap <beleap@beleap.dev>
2023-11-13 13:18:41 +0900

    add iron.nvim

--- users/beleap/files/.config/nvim/lua/plugins/language-support.lua
+++ users/beleap/files/.config/nvim/lua/plugins/language-support.lua
@@ -100,4 +100,41 @@ return {
     },
     config = true,
   },
+  {
+    "Vigemus/iron.nvim",
+    event = "VeryLazy",
+    config = function()
+      local iron = require("iron.core")
+      iron.setup({
+        config = {
+          scratch_repl = true,
+          repl_definition = {
+            sh = {
+              command = {"bash"}
+            }
+          },
+          repl_open_cmd = require("iron.view").bottom(30),
+        },
+        keymaps = {
+          send_motion = "<space>sc",
+          visual_send = "<space>sc",
+          send_file = "<space>sf",
+          send_line = "<space>sl",
+          send_until_cursor = "<space>su",
+          send_mark = "<space>sm",
+          mark_motion = "<space>mc",
+          mark_visual = "<space>mc",
+          remove_mark = "<space>md",
+          cr = "<space>s<cr>",
+          interrupt = "<space>s<space>",
+          exit = "<space>sq",
+          clear = "<space>cl",
+        },
+        highlight = {
+          italic = true
+        },
+        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      })
+    end
+  },
 }
