9786bf25248fe22d9b703424af05743e58665bfd
BeLeap <beleap@beleap.dev>
2023-12-05 10:06:28 +0900

    remove neovim nightly

--- flake.nix
+++ flake.nix
@@ -12,14 +12,11 @@
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
 
     nixgl.url = "github:guibou/nixGL";
-
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = inputs@{ home-manager, nixpkgs, ... }:
     let
       overlays = [
-        # inputs.neovim-nightly-overlay.overlay
         inputs.nixgl.overlay
       ];
     in
