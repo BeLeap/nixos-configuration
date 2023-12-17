bfcee10855937ef9428de43c0a2121ee595f5c4b
BeLeap <beleap@beleap.dev>
2023-12-17 16:57:50 +0900

    share config

--- users/beleap/files/.config/nvim/lua/plugins/language-support.lua
+++ users/beleap/files/.config/nvim/lua/plugins/language-support.lua
@@ -160,6 +160,22 @@ return {
     'https://codeberg.org/esensar/nvim-dev-container',
     dependencies = 'nvim-treesitter/nvim-treesitter',
     event = "VeryLazy",
-    config = true,
+    opts = {
+      attach_mounts = {
+        neovim_config = {
+          enabled = true,
+          options = { "readonly" }
+        },
+        neovim_data = {
+          enabled = false,
+          options = {}
+        },
+        -- Only useful if using neovim 0.8.0+
+        neovim_state = {
+          enabled = false,
+          options = {}
+        },
+      },
+    },
   },
 }
