0596483d5b628780c12e9367ac20903000b626bb
BeLeap <beleap@beleap.dev>
2023-12-26 23:13:27 +0900

    Enable wlr in XDG portal

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -99,10 +99,14 @@
   xdg = {
     portal = {
       enable = true;
+      wlr = {
+        enable = true;
+      };
       extraPortals = with pkgs; [
         xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
       ];
+      xdgOpenUsePortal = true;
       config = {
         common = {
           default = ["wlr"];
