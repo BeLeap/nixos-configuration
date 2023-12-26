6fb981465b8a1dc9b27adaa11c4878d4775edf28
BeLeap <beleap@beleap.dev>
2023-12-26 19:18:20 +0900

    Changed the interpreter for `alert.sh` from `/usr/bin/env bash` to `/run/current-system/sw/bin/bash` and added the paths from the current Nix profile to the `PATH` variable.

--- users/beleap/files/.scripts/alert.sh
+++ users/beleap/files/.scripts/alert.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/run/current-system/sw/bin/bash
 
+PATH=/run/wrappers/bin:/home/beleap/.nix-profile/bin:/nix/profile/bin:/home/beleap/.local/state/nix/profile/bin:/etc/profiles/per-user/beleap/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin
 SOUND_ROOT=$(nix path-info "nixpkgs#sound-theme-freedesktop" --json | jq -r '.[].path')
 mpv $SOUND_ROOT/share/sounds/freedesktop/stereo/message.oga
