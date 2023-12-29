792d484e4378707c26990866adc17dfac4799357
BeLeap <beleap@beleap.dev>
2023-12-29 16:33:36 +0900

    Added keybindings to switch between panes while in vim.

--- users/beleap/programs/tui/default.nix
+++ users/beleap/programs/tui/default.nix
@@ -29,6 +29,18 @@
       set -g base-index 1
       set-option -g default-terminal "screen-256color"
       set-option -sa terminal-features ',XXX:RGB'
+
+      is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?\.?(view|n?vim?x?)(-wrapped)?(diff)?$'"
+
+      bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h' { if -F '#{pane_at_left}' '''''' 'select-pane -L' }
+      bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j' { if -F '#{pane_at_bottom}' '''''' 'select-pane -D' }
+      bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k' { if -F '#{pane_at_top}' '''''' 'select-pane -U' }
+      bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l' { if -F '#{pane_at_right}' '''''' 'select-pane -R' }
+
+      bind-key -T copy-mode-vi 'C-h' if -F '#{pane_at_left}' '''''' 'select-pane -L'
+      bind-key -T copy-mode-vi 'C-j' if -F '#{pane_at_bottom}' '''''' 'select-pane -D'
+      bind-key -T copy-mode-vi 'C-k' if -F '#{pane_at_top}' '''''' 'select-pane -U'
+      bind-key -T copy-mode-vi 'C-l' if -F '#{pane_at_right}' '''''' 'select-pane -R'
     '';
 
     tmuxinator = {
