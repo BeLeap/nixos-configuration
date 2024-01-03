329cdd8598c34c84a7102e9bfcdfd809a1b5c58b
BeLeap <beleap@beleap.dev>
2024-01-03 20:14:09 +0900

    Add beleap user to default session

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -91,6 +91,7 @@
       settings = {
         default_session = {
           command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'sway --unsupported-gpu'";
+          user = "beleap";
         };
       };
     };
