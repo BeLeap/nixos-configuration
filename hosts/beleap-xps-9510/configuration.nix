3d979303028e0a898825605227092bc07b553dc1
BeLeap <beleap@beleap.dev>
2024-01-07 18:47:32 +0900

    Enable Steam remote play and dedicated server and open firewall ports.

--- hosts/beleap-xps-9510/configuration.nix
+++ hosts/beleap-xps-9510/configuration.nix
@@ -55,5 +55,13 @@ rec {
 
       enableNvidia = true;
   };
+
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+    };
+  };
 }
 
