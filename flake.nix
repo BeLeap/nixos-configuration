a06d924e1995c3286cdd36b424959f2889172b8e
BeLeap <beleap@beleap.dev>
2024-01-10 12:46:46 +0900

    format

--- flake.nix
+++ flake.nix
@@ -26,23 +26,25 @@
     in
     {
       nixosConfigurations = fold
-        (acc: elem: acc // { "${elem}" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            { nixpkgs.overlays = overlays; }
-            (./. + "/hosts/${elem}/configuration.nix")
-            home-manager.nixosModules.home-manager
-            ({ pkgs, lib, ... }: {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.beleap = import ./users/beleap/home.nix {
-                inherit pkgs lib;
-                hostname = elem;
-              };
-            })
-          ];
-        }; } )
-        {} [ "beleap-xps-9510" "beleap-thinkpad" ];
+        (acc: elem: acc // {
+          "${elem}" = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              { nixpkgs.overlays = overlays; }
+              (./. + "/hosts/${elem}/configuration.nix")
+              home-manager.nixosModules.home-manager
+              ({ pkgs, lib, ... }: {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.beleap = import ./users/beleap/home.nix {
+                  inherit pkgs lib;
+                  hostname = elem;
+                };
+              })
+            ];
+          };
+        })
+        { } [ "beleap-xps-9510" "beleap-thinkpad" ];
 
       homeConfigurations = {
         beleap = import ./users/beleap {
