24fe34b3ed3385ad08cbd6e9bfc90b0e3fcf8011
BeLeap <beleap@beleap.dev>
2024-01-11 09:37:30 +0900

    add model.nvim

--- users/beleap/files/.config/nvim/lua/plugins/init.lua
+++ users/beleap/files/.config/nvim/lua/plugins/init.lua
@@ -280,4 +280,21 @@ return {
       },
     },
   },
+  {
+    'gsuuon/model.nvim',
+    cmd = { 'M', 'Model', 'Mchat' },
+    init = function()
+      vim.filetype.add({
+        extension = {
+          mchat = 'mchat',
+        }
+      })
+    end,
+    ft = 'mchat',
+    keys = {
+      { '<C-m>d',       ':Mdelete<cr>', mode = 'n' },
+      { '<C-m>s',       ':Mselect<cr>', mode = 'n' },
+      { '<C-m><space>', ':Mchat<cr>',   mode = 'n' }
+    },
+  },
 }
