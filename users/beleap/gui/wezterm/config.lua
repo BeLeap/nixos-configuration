20a211bfd511533c601c4043bdba6ada347099f1
BeLeap <beleap@beleap.dev>
2024-01-12 17:11:36 +0900

    style(gui): Switch color scheme to Nord

--- users/beleap/gui/wezterm/config.lua
+++ users/beleap/gui/wezterm/config.lua
@@ -5,7 +5,7 @@ return {
     harfbuzz_features = { "calt", "liga", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "zero", "onum" },
   },
   font_size = 14.0,
-  color_scheme = "Catppuccin Mocha",
+  color_scheme = "nord",
   enable_tab_bar = false,
   check_for_updates = false,
   warn_about_missing_glyphs = false,
