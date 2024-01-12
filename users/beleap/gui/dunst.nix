36c7fbf812564af83645a4ad79987e16a0729a4e
BeLeap <beleap@beleap.dev>
2024-01-12 17:46:00 +0900

    chore(dunst): change theme to Nord

--- users/beleap/gui/dunst.nix
+++ users/beleap/gui/dunst.nix
@@ -1,15 +1,15 @@
 { pkgs }:
 let
-  catppuccin = import ../../../const/catppuccin.nix;
+  nord = import ../../../const/nord.nix;
 in
 {
   enable = true;
 
   settings = {
     global = {
-      background = catppuccin.base;
-      foreground = catppuccin.text;
-      frame_color = catppuccin.lavender;
+      background = nord.nord0;
+      foreground = nord.nord4;
+      frame_color = nord.nord8;
       offset = "0x0";
       corner_radius = 10;
       frame_width = 4;
@@ -24,7 +24,7 @@ in
       timeout = 10;
     };
     urgency_critical = {
-      frame_color = catppuccin.peach;
+      frame_color = nord.nord11;
       timeout = 0;
     };
     play_sound = {
