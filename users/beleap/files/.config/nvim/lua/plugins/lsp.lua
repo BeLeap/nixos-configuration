48529945cfc0159d537c2b2fa66cfd084dde127f
BeLeap <beleap@beleap.dev>
2024-01-18 13:23:21 +0900

    Remove iron.nvim

--- users/beleap/files/.config/nvim/lua/plugins/lsp.lua
+++ users/beleap/files/.config/nvim/lua/plugins/lsp.lua
@@ -100,45 +100,6 @@ return {
     config = true,
   },
   {
-    "Vigemus/iron.nvim",
-    event = "VeryLazy",
-    config = function()
-      local iron = require("iron.core")
-      iron.setup({
-        config = {
-          scratch_repl = true,
-          repl_definition = {
-            sh = {
-              command = { "bash" }
-            }
-          },
-          repl_open_cmd = require('iron.view').right(function()
-            return vim.o.columns / 2
-          end),
-        },
-        keymaps = {
-          send_motion = "<space>sc",
-          visual_send = "<space>sc",
-          send_file = "<space>sf",
-          send_line = "<space>sl",
-          send_until_cursor = "<space>su",
-          send_mark = "<space>sm",
-          mark_motion = "<space>mc",
-          mark_visual = "<space>mc",
-          remove_mark = "<space>md",
-          cr = "<space>s<cr>",
-          interrupt = "<space>s<space>",
-          exit = "<space>sq",
-          clear = "<space>cl",
-        },
-        highlight = {
-          italic = true
-        },
-        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-      })
-    end
-  },
-  {
     "j-hui/fidget.nvim",
     event = "VeryLazy",
     opts = {
