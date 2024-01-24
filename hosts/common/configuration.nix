1285541dd062bbe8e13ba5530aefbc420bd1217c
BeLeap <beleap@beleap.dev>
2024-01-25 00:05:29 +0900

    setup nameservers

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -22,12 +22,17 @@
       };
     };
   };
-  networking = {
-    networkmanager = {
-      enable = true;
-      insertNameservers = [ "1.1.1.1" "1.0.0.1" ];
+  networking =
+    let
+      nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    in
+    {
+      networkmanager = {
+        enable = true;
+        insertNameservers = nameservers;
+      };
+      nameservers = nameservers;
     };
-  };
 
   time.timeZone = "Asia/Seoul";
 
