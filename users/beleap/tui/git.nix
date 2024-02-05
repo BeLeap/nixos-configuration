2abc7421653498331cd517fa1addbcdadaa4d6d8
BeLeap <beleap@beleap.dev>
2024-02-05 14:57:17 +0900

    feat: enable signing commits with op-ssh-sign

--- users/beleap/tui/git.nix
+++ users/beleap/tui/git.nix
@@ -8,8 +8,10 @@
     push.autoSetupRemote = true;
     init.defaultBranch = "main";
     user.signingKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNrBVLG52+DGp9mHdt5H45alxWVCLu5JsodOPryPar5R620vqryWONGsE9FI8EeFRiBvIvNhhvYlbTzaSYU46koVRAUcjFUH3Wd3NW15sY4UiC9RYVMMtc0IpghSTa0cPH06XvU0d8cftySfarsT6rlJPLDpOKKn0yrbfI3ErncLpIWyBIYELkzJ3azeb4J8L2KoO+l4Ce4QR7E1eyqRXOPT81ZwK19mTW/F+H+UAlcQrv5uUh3NZclmahE3Vwc23VWORwmBvHnhcZSOb/M79lk45WVUFZYPBqSPxihNd9Cpcq7TgKczS1liiO2S2NIJ73wG/UviuR52fOqBagJBlL";
-    gpg.format = "ssh";
-    gpg.ssh.program = "op-ssh-sign";
+    gpg = {
+      format = "ssh";
+      ssh.program = "op-ssh-sign";
+    };
     commit.gpgsign = true;
     rerere.enable = true;
   };
