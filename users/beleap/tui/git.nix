87a11ebec0980f27521c81ad043efc8160bd4a19
BeLeap <beleap@beleap.dev>
2024-02-05 15:16:39 +0900

    feat: add `adog` alias to log all commits

--- users/beleap/tui/git.nix
+++ users/beleap/tui/git.nix
@@ -32,5 +32,6 @@
   aliases = {
     gone = "! git fetch -p && git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '\$2 == \"[gone]\" {print \$1}' | xargs -r git branch -D";
     staash = "stash --all";
+    adog = "log --all --decorate --oneline --graph";
   };
 }
