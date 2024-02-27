924a1c2c177aa26d4498d452d96a62420b91cebc
BeLeap <beleap@beleap.dev>
2024-02-27 18:13:13 +0900

    feat: recover xplr default config

--- users/beleap/files/.config/xplr/init.lua
+++ users/beleap/files/.config/xplr/init.lua
@@ -1,16 +1,2 @@
 version = '0.21.5'
 xplr.config.general.show_hidden = true
-xplr.config.layouts.builtin.default = {
-  Vertical = {
-    config = {
-      constraints = {
-        { Percentage = 50 },
-        { Percentage = 50 },
-      },
-    },
-    splits = {
-      "Table",
-      "Selection",
-    },
-  },
-}
