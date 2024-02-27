d4bc0484d13d817a466817ac6075631f44c1ee94
BeLeap <beleap@beleap.dev>
2024-02-27 18:28:13 +0900

    feat: add edit keybinding

--- users/beleap/files/.config/xplr/init.lua
+++ users/beleap/files/.config/xplr/init.lua
@@ -1,2 +1,20 @@
 version = '0.21.5'
 xplr.config.general.show_hidden = true
+
+xplr.config.modes.builtin.default = {
+  key_bindings = {
+    on_key = {
+      e = {
+        help = "Edit",
+        messages = {
+          {
+            BashExec0 = [===[
+              ${EDITOR:-vi} "${XPLR_FOCUS_PATH:?}"
+            ]===],
+          },
+          "PopMode",
+        },
+      },
+    },
+  },
+}
