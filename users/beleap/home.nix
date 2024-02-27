7c066a3da7c536eb28e5289ec5ac8dced1c58f90
BeLeap <beleap@beleap.dev>
2024-02-27 16:20:02 +0900

    refact: separate inputMethod configuration

--- users/beleap/home.nix
+++ users/beleap/home.nix
@@ -126,25 +126,5 @@ in
     );
   };
 
-  i18n.inputMethod = {
-    enabled = "kime";
-    kime.config = {
-      indicator.icon_color = "White";
-      engine = {
-        latin = {
-          layout = "Colemak";
-        };
-        hangul = {
-          layout = "sebeolsik-3-90";
-          addons = {
-            "sebeolsik-3-90" = [
-              "FlexibleComposeOrder"
-              "ComposeChoseongSsang"
-              "ComposeJongseongSsang"
-            ];
-          };
-        };
-      };
-    };
-  };
+  i18n.inputMethod = (import ./inputMethod.nix);
 }
