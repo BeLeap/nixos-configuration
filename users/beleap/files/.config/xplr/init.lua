dc9ac319835a912ce49da54632e36cf22e200895
BeLeap <beleap@beleap.dev>
2024-02-27 17:46:34 +0900

    feat: add xplr config

---
+++ users/beleap/files/.config/xplr/init.lua
@@ -0,0 +1,16 @@
+xplr.config.layouts.builtin.default = {
+  Horizontal = {
+    config = {
+      margin = 1,
+      horizontal_margin = 1,
+      vertical_margin = 1,
+      constraints = {
+        { Percentage = 50 },
+        { Percentage = 50 },
+      }
+    },
+    splits = {
+      "Table",
+    }
+  }
+}
