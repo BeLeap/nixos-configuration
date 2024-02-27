894e8ba77bc5640597a9a138b2adc72799fe5f96
BeLeap <beleap@beleap.dev>
2024-02-27 16:53:06 +0900

    fix: remove tmux vi integration

--- users/beleap/tui/tmux.nix
+++ users/beleap/tui/tmux.nix
@@ -42,31 +42,6 @@
     # Use y to yank current selection
     bind-key -T copy-mode-vi y send-keys -X copy-selection
 
-
-    is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?\.?(view|n?vim?x?)(-wrapped)?(diff)?$'"
-
-    bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h' { if -F '#{pane_at_left}' '''''' 'select-pane -L' }
-    bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j' { if -F '#{pane_at_bottom}' '''''' 'select-pane -D' }
-    bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k' { if -F '#{pane_at_top}' '''''' 'select-pane -U' }
-    bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l' { if -F '#{pane_at_right}' '''''' 'select-pane -R' }
-
-    bind-key -T copy-mode-vi 'C-h' if -F '#{pane_at_left}' '''''' 'select-pane -L'
-    bind-key -T copy-mode-vi 'C-j' if -F '#{pane_at_bottom}' '''''' 'select-pane -D'
-    bind-key -T copy-mode-vi 'C-k' if -F '#{pane_at_top}' '''''' 'select-pane -U'
-    bind-key -T copy-mode-vi 'C-l' if -F '#{pane_at_right}' '''''' 'select-pane -R'
-  
-    is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-
-    bind -n 'M-h' if-shell "$is_vim" 'send-keys M-h' 'resize-pane -L 1'
-    bind -n 'M-j' if-shell "$is_vim" 'send-keys M-j' 'resize-pane -D 1'
-    bind -n 'M-k' if-shell "$is_vim" 'send-keys M-k' 'resize-pane -U 1'
-    bind -n 'M-l' if-shell "$is_vim" 'send-keys M-l' 'resize-pane -R 1'
-
-    bind-key -T copy-mode-vi M-h resize-pane -L 1
-    bind-key -T copy-mode-vi M-j resize-pane -D 1
-    bind-key -T copy-mode-vi M-k resize-pane -U 1
-    bind-key -T copy-mode-vi M-l resize-pane -R 1
-
     bind f run-shell '~/.scripts/tmux-scratch.sh'
 
     bind r source-file ~/.config/tmux/tmux.conf
